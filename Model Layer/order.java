79c7c5f7a4dc381beabd36414a8a44ce72d4a3c6
Amarjyotigithub <amaraj38841@gmail.com>
2026-04-06 22:06:33 +0530

    Added full project structure

---
+++ Model Layer/order.java
@@ -0,0 +1,17 @@
+package com.example.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+    private double totalAmount;
+}
